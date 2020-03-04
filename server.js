382d89cd39f2ef9c1846c7c9a603dfd420abfbc6
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-04 00:06:13 -0800

    create a mock db of authors and books

--- server.js
+++ server.js
@@ -1,25 +1,57 @@
 const express = require("express");
 const expressGraphQL = require("express-graphql");
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLList
+} = require("graphql");
 
 const PORT = 5000;
 
 const app = express();
 
-// create schema
-const schema = new GraphQLSchema({
-  // example model: create an object that will query for an object called name: "Hello"
-  // the object has a field called message
-  query: new GraphQLObjectType({
-    name: "Hello",
-    fields: () => ({
-      message: {
-        // define the type of the message
-        type: GraphQLString, // define data type of this message
-        resolve: () => "Hello Hello" // tells graphql where to get the information
-      }
-    })
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  description: "Single book by author",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) }, // define type with graphql api
+    name: { type: GraphQLNonNull(GraphQLString) },
+    authorId: { type: GraphQLNonNull(GraphQLInt) }
   })
+});
+
+const RootQueryType = new GraphQLObjectType({
+  name: "Query", // namae the query
+  description: "Root Query", // give a desc to the query
+  fields: () => ({
+    books: {
+      type: new GraphQLList(BookType), // create a type
+      description: "List of books",
+      resolve: () => books
+    }
+  })
+});
+
+// Example database
+const books = [
+  { id: 1, name: "Harry Potter and the Chamber of Secrets", authorId: 1 },
+  { id: 2, name: "Harry Potter and the Prisoner of Alibaba", authorId: 1 },
+  { id: 3, name: "Harry Potter and the Goblet of Stout", authorId: 1 },
+  { id: 4, name: "LOTR: The Fellowship of the Ring", authorId: 2 },
+  { id: 5, name: "LOTR: The Two Towers", authorId: 2 },
+  { id: 5, name: "LOTR: The Return of the King", authorId: 2 }
+];
+
+const authors = [
+  { id: 1, name: "J. K. Rowling" },
+  { id: 2, name: "J. R. R. Tolkien" }
+];
+
+const schema = new GraphQLSchema({
+  query: RootQueryType
 });
 
 // pass in an object containing the schema and UI to graphql
